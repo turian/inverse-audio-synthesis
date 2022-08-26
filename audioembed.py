811550de8e2f02dff61f1396a24b98893af65cc5
Joseph Turian <turian@gmail.com>
2022-08-26 07:35:00 +0000

    best stft

--- audioembed.py
+++ audioembed.py
@@ -41,26 +41,26 @@ class AudioEmbedding(nn.Module):
         x = audio
 
         # PQMF is 3 channel
-#        z = self.gram(audio)
+        #        z = self.gram(audio)
 
         # Three channels of mel in different ways
         zlin = self.gram(audio)
         zlog1 = torch.log(zlin + 1e-6)
         zlog2 = torch.log10(zlin + 1e-2)
-        z = torch.stack([zlin, zlog1, zlog2]).permute(1,0,2,3)
+        z = torch.stack([zlin, zlog1, zlog2]).permute(1, 0, 2, 3)
 
         z = self.batchnorm(z)
 
-#        # This will be [128, 3, 58800]
-#        zimg = z.reshape(-1, 3, 240, 245)
-##        # Convert float to unsigned bytes
-##        zimg8 = scale8(zimg)
-##        # torchvision 0.12.0
-##        zimg8 = zimg8.float() / 255.0
-#        zimg8 = zimg
+        #        # This will be [128, 3, 58800]
+        #        zimg = z.reshape(-1, 3, 240, 245)
+        ##        # Convert float to unsigned bytes
+        ##        zimg8 = scale8(zimg)
+        ##        # torchvision 0.12.0
+        ##        zimg8 = zimg8.float() / 255.0
+        #        zimg8 = zimg
 
-#        # Apply inference preprocessing transforms
-#        zimg8preprocess = self.img_preprocess(zimg8)
+        #        # Apply inference preprocessing transforms
+        #        zimg8preprocess = self.img_preprocess(zimg8)
 
         zimg8preprocess = self.img_preprocess(z)
 
