815836ae161ef9e8863319028d1dbcbf1ea22665
Joseph Turian <turian@gmail.com>
2022-09-13 07:19:41 +0200

    Cleanup fixes

--- wandb-cleanup.py
+++ wandb-cleanup.py
@@ -4,23 +4,30 @@
 # method on artifact_collection."
 
 import wandb
+from tqdm.auto import tqdm
 
-dry_run = True
-#api = wandb.Api(overrides={"project": "vicreg-synth1b1-pqmfs", "entity": "turian"})
+# dry_run = True
+dry_run = False
+# api = wandb.Api(overrides={"project": "vicreg-synth1b1-pqmfs", "entity": "turian"})
 api = wandb.Api()
-project = api.project('vicreg-synth1b1-pqmfs')
+project = api.project("vicreg-synth1b1-pqmfs")
 
 for artifact_type in project.artifacts_types():
-    for artifact_collection in artifact_type.collections():        
+    if artifact_type.type != "model":
+        continue
+    collection_versions = []
+    for artifact_collection in tqdm(artifact_type.collections()):
         for version in artifact_collection.versions():
-            if artifact_type.type == 'model':
-                if len(version.aliases) > 0:
-                    # print out the name of the one we are keeping
-                    print(f'KEEPING {version.name} {(version.aliases, artifact_collection.name, artifact_type.type)}')
-                else:
-                    print(f'DELETING {version.name}')
-                    if not dry_run:
-                        print('')
-                        version.delete()
+            if version.state != "DELETED":
+                collection_versions.append((artifact_collection, version))
 
-
+for (artifact_collection, version) in tqdm(collection_versions):
+    if len(version.aliases) > 0:
+        # print out the name of the one we are keeping
+        print(f"KEEPING {version.name} {version.aliases}")
+    else:
+        if not dry_run:
+            version.delete()
+        else:
+            print("")
+            print(f"should delete {version.name}")
